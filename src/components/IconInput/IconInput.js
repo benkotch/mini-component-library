c1bdae30ab1482b477b70f93e47de0d3397f46b9
Ben Kotch <benkotch@mac.com>
2021-10-09 12:32:26 -0700

    after watching video

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,30 +9,34 @@ import VisuallyHidden from "../VisuallyHidden";
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   const SIZES = {
     icon: {
-      small: 14,
-      large: 18,
+      small: 16,
+      large: 24,
     },
     input: {
       small: {
-        "--font-size": 14 + "px",
+        "--font-size": 14 / 16 + "rem",
 
         "--padding": "0 0 0 20px",
-        "--border-bottom-width": 1 + "px",
+        "--border-bottom-width": 1 + "rem",
+        "--height": 24 + "px",
       },
       large: {
-        "--font-size": 18 + "px",
-        "--padding": "0 0 2px 30px",
+        "--font-size": 18 / 16 + "rem",
+        "--padding": "0 0 0 30px",
         "--border-bottom-width": 2 + "px",
+        "--height": 36 + "px",
       },
     },
   };
 
   return (
     <ElementWrapper>
-      <VisuallyHidden>
-        <label>{label}</label>
-      </VisuallyHidden>
-      <IconWrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper
+        style={{
+          "--size": SIZES.icon[size] + "px",
+        }}
+      >
         <Icon id={icon} size={SIZES.icon[size]} strokeWidth={2} />
       </IconWrapper>
       <InputWrapper
@@ -45,8 +49,10 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   );
 };
 
-// Hack to get icon to be same color on hover
-const ElementWrapper = styled.div`
+const ElementWrapper = styled.label`
+  position: relative;
+  width: max-content;
+
   color: ${COLORS.gray700};
   :hover {
     color: ${COLORS.black};
@@ -56,19 +62,22 @@ const ElementWrapper = styled.div`
 const InputWrapper = styled.input`
   border: none;
   border-bottom: 1px solid ${COLORS.black};
+  border-bottom-width: var(--border-bottom-width);
   background-color: transparent;
-  color: currentColor;
+  color: inherit;
   font-weight: 700;
   padding: var(--padding);
+  height: var(--height);
   width: ${(props) => props.width + "px"};
   min-width: 100px;
   max-width: 300px;
-
+  outline-offset: 2px;
   font-family: Roboto;
   font-size: var(--font-size);
   line-height: 21px;
 
-  ::placholder {
+  &::placeholder {
+    font-weight: 400;
     color: ${COLORS.gray500};
   }
   ::-webkit-input-placeholder,
@@ -80,10 +89,18 @@ const InputWrapper = styled.input`
 `;
 
 const IconWrapper = styled.div`
-  display: inline-block;
+  /* display: inline-block;
   margin-right: -18px;
   margin-bottom: -2px;
-  color: currentColor;
+  color: currentColor; */
+  position: absolute;
+
+  height: var(--size);
+  width: var(--size);
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto 0;
 `;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,51 +7,114 @@ import { getDisplayedValue } from "./Select.helpers";
 
 // TODO: dynamic width on select, icon isn't clickable
 // part way there but not all the way
+// I tried making native select hidden but then it wasn't clickable,
+// looking foward to Josh's solution
 
-const SelectElement = styled.select`
-  appearance: none;
-  border: none;
+// const SelectElement = styled.select`
+//   appearance: none;
+//   border: none;
+//   border-radius: 8px;
+//   background-color: ${COLORS.gray50};
+//   color: currentColor;
+//   font-family: "Roboto", sans-serif;
+//   font-weight: normal;
+//   font-size: ${16 / 16 + "rem"};
+//   line-height: ${19 / 16 + "rem"};
+//   padding: 12px 24px 12px 16px;
+//   width: auto;
+// `;
+
+// const Wrapper = styled.div`
+//   position: relative;
+//   width: fit-content;
+//   color: ${COLORS.gray700};
+
+//   &:hover {
+//     color: ${COLORS.black};
+//   }
+// `;
+
+// const IconWrapper = styled.div`
+//   position: absolute;
+//   top: 14px;
+//   right: 10px;
+// `;
+
+const Select = ({ label, value, onChange, children }) => {
+  const displayedValue = getDisplayedValue(value, children);
+
+  // return (
+  //   <Wrapper>
+  //     <SelectElement value={value} onChange={onChange}>
+  //       {children}
+  //     </SelectElement>
+  //     <IconWrapper>
+  //       <Icon id="chevron-down" size={14} strokeWidth={3} />
+  //     </IconWrapper>
+  //   </Wrapper>
+  // );
+
+  // from Josh's video
+
+  return (
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" size={24} strokeWidth={1} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
   border-radius: 8px;
-  background-color: ${COLORS.gray50};
-  color: currentColor;
+  color: ${COLORS.gray700};
   font-family: "Roboto", sans-serif;
   font-weight: normal;
   font-size: ${16 / 16 + "rem"};
   line-height: ${19 / 16 + "rem"};
-  padding: 12px 24px 12px 16px;
-  width: auto;
-`;
+  padding: 12px 16px;
+  padding-right: 52px;
 
-const Wrapper = styled.div`
-  position: relative;
-  width: fit-content;
-  color: ${COLORS.gray700};
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
 
-  &:hover {
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
   }
 `;
 
 const IconWrapper = styled.div`
   position: absolute;
-  top: 14px;
+  height: var(--size);
+  width: var(--size);
+  top: 0;
+  bottom: 0;
   right: 10px;
+  margin: auto;
+  pointer-events: none;
 `;
-
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-  console.log(displayedValue);
-
-  return (
-    <Wrapper>
-      <SelectElement value={value} onChange={onChange}>
-        {children}
-      </SelectElement>
-      <IconWrapper>
-        <Icon id="chevron-down" size={14} strokeWidth={3} />
-      </IconWrapper>
-    </Wrapper>
-  );
-};
 
 export default Select;
